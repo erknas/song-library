e796bd342867c1bf51f10ce636cf0f9f4332cb03
Aleksandr <vin89441@gmail.com>
2025-02-04 19:05:51 +0300

    add UpdateSongRequest struct

--- internal/types/requests.go
+++ internal/types/requests.go
@@ -4,3 +4,11 @@ type SongRequest struct {
 	Song  string `json:"song"`
 	Group string `json:"group"`
 }
+
+type UpdateSongRequest struct {
+	Song        string `json:"song"`
+	Group       string `json:"group"`
+	ReleaseDate string `json:"releaseDate"`
+	Text        string `json:"text"`
+	Link        string `json:"link"`
+}
